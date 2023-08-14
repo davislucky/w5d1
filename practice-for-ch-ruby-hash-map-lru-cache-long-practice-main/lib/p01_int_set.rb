4751e157a02eef840b06790b5d788a15166a8e90
Brittiny Filbert <britfilbert@icloud.com>
2023-08-14 14:56:40 -0700

    Finished IntSet MaxIntSet

--- practice-for-ch-ruby-hash-map-lru-cache-long-practice-main/lib/p01_int_set.rb
+++ practice-for-ch-ruby-hash-map-lru-cache-long-practice-main/lib/p01_int_set.rb
@@ -1,14 +1,23 @@
 class MaxIntSet
+
+  attr_accessor :store
   def initialize(max)
+    @max = max
+    @store = Array.new(max, false)
   end
 
   def insert(num)
+    raise "Out of bounds" if num > @max || num < 0
+
+    @store[num] = true
   end
 
   def remove(num)
+    @store[num] = false
   end
 
   def include?(num)
+    @store[num]
   end
 
   private
@@ -26,18 +35,25 @@ class IntSet
   end
 
   def insert(num)
+    self[num % 20] << num #if self[num] 
+
+    # p @store
   end
 
   def remove(num)
+    self[num % 20].delete(num)
   end
 
   def include?(num)
+    self[num % 20].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num]
+
   end
 
   def num_buckets
